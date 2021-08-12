2ac3a42ab9281f153f1746395d925118d7e78299
Alex Wardill <alexander@wardill.net>
2021-08-12 19:10:35 +0100

    some functionality

--- app.js
+++ app.js
@@ -1,15 +1,46 @@
 const checked = [];
 
-const checkboxes = Array.from(document.querySelectorAll('input[type="checkbox"]'));
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+const checkboxesArr = Array.from(document.querySelectorAll('input[type="checkbox"]'));
+let multiple = false;
 
-checkboxes.forEach((e) => {
-    e.addEventListener('change', isChecked(e.target));
-    console.log(checked);
+function uncheckAll() {
+    checkboxes.forEach((e) => e.checked = false);
+}
+
+document.addEventListener('keydown', (e) => {
+    if (e.key == 'Shift') {
+        multiple = true;
+        console.log(multiple);
+    }
 });
 
+document.addEventListener('keyup', (e) => {
+    if (e.key == 'Shift') {
+        multiple = false;
+        console.log(multiple);
+    }
+});
 
-function isChecked(item) {
-    if (item.checked) {
-        checked.push(item);
-    } checked.splice(checked[item]);
+// if there is another check box
+//  when you shift check 
+
+checkboxes.forEach((box) => box.addEventListener(('change'), (e) => {
+    if (multiple == true) {
+        isPreviousChecked(e.target);
+        //e.target.parentElement.previousSibling.previousSibling.firstElementChild.checked = true;
+    }
+}));
+
+function isPreviousChecked(box) {
+    if (box.parentElement.previousSibling.previousSibling.firstElementChild == null) {
+        return;
+    }   else if (box.parentElement.previousSibling.previousSibling.firstElementChild.checked == false) {
+        box.parentElement.previousSibling.previousSibling.firstElementChild.checked = true;
+        isPreviousChecked(box.parentElement.previousSibling.previousSibling.firstElementChild);
+    }
 }
+
+
+
+
